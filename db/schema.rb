8c4a5d423bc788a1a550aa310363f8d3c1b021e9
Sean Yeoh <sean.yeoh@hotmail.com>
2017-06-20 10:43:10 +0800

    Add colors scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170619090751) do
+ActiveRecord::Schema.define(version: 20170620023451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 20170619090751) do
   create_table "blanks", force: :cascade do |t|
     t.string   "name"
     t.string   "brand"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "colors", force: :cascade do |t|
+    t.string   "name"
+    t.string   "hex"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
