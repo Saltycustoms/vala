e1d7f8a2747fac04c81c7be1684d90a2906b03d1
Sean Yeoh <sean.yeoh@hotmail.com>
2017-06-21 10:15:00 +0800

    Add dependent destroy for color options, add responsive class for checkbox

--- app/models/color.rb
+++ app/models/color.rb
@@ -1,6 +1,6 @@
 class Color < ApplicationRecord
   validates :name, presence: true
   validates :hex, presence: true
-  has_many :color_options
+  has_many :color_options, dependent: :destroy
   has_many :products, through: :color_options
 end
