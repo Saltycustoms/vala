1e50344d662829d8486661cae5faa182717f9c72
Sean Yeoh <sean.yeoh@hotmail.com>
2017-06-22 10:49:43 +0800

    Remove send method for resource class as resource class is already constantize

--- app/controllers/api/v1/api_v1_controller.rb
+++ app/controllers/api/v1/api_v1_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::ApiV1Controller < ApplicationController
   before_action :set_resource, only: [:show, :edit, :update, :destroy]
 
   def index
-    @resources = resource_class.send(:all)
+    @resources = resource_class.all
     render json: @resources
   end
 
@@ -13,7 +13,7 @@ class Api::V1::ApiV1Controller < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_resource
-      @resource = resource_class.send(:find_by, id: params[:id])
+      @resource = resource_class.find_by(id: params[:id])
     end
 
     def resource_class
