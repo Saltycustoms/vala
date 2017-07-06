5a6ab79b505c41a1c62b48a0d01a14709b3df66c
Sean Yeoh <sean.yeoh@hotmail.com>
2017-07-06 14:27:39 +0800

    Update blank form to include prints, properties and sides nested forms.

--- app/controllers/blanks_controller.rb
+++ app/controllers/blanks_controller.rb
@@ -1,7 +1,8 @@
 class BlanksController < ApplicationController
   dried_options ({
     fields:
-      [:name, :brand, sides_attributes: [:id, :name, :attachment, :attachment_data, :_destroy]],
+      [:name, :brand, sides_attributes: [:id, :name, :attachment, :attachment_data, :_destroy], properties_attributes: [:id, :prop_type, :method, :name, :element, :back_element_id, :_destroy],
+        full_prints_attributes: [:id, :name, :element_id, :_destroy], sleeve_prints_attributes: [:id, :name, :left_half_element_id, :right_half_element_id, :_destroy]],
     presentation:
       [:id, :name, :brand]
   })
--- app/models/blank.rb
+++ app/models/blank.rb
@@ -1,8 +1,14 @@
 class Blank < ApplicationRecord
   has_many :sides, dependent: :destroy
-  has_many :properties
-  has_many :prints
   accepts_nested_attributes_for :sides, allow_destroy: true, reject_if: proc { |attributes| attributes.all? { |key, value| key == "_destroy" || value.blank? } }
+  has_many :properties, dependent: :destroy
+  accepts_nested_attributes_for :properties, allow_destroy: true, reject_if: proc { |attributes| attributes.all? { |key, value| key == "_destroy" || value.blank? } }
+  has_many :prints, dependent: :destroy
+  accepts_nested_attributes_for :sides, allow_destroy: true, reject_if: proc { |attributes| attributes.all? { |key, value| key == "_destroy" || value.blank? } }
+  has_many :full_prints, class_name: "Print::FullPrint", dependent: :destroy
+  accepts_nested_attributes_for :full_prints, allow_destroy: true, reject_if: proc { |attributes| attributes.all? { |key, value| key == "_destroy" || value.blank? } }
+  has_many :sleeve_prints, class_name: "Print::SleevePrint", dependent: :destroy
+  accepts_nested_attributes_for :sleeve_prints, allow_destroy: true, reject_if: proc { |attributes| attributes.all? { |key, value| key == "_destroy" || value.blank? } }
   validates :name, presence: true
   validates :brand, presence: true
 end
