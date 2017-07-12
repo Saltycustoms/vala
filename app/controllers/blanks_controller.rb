f2a11070fec4d5ada3550726e44d1108f11e1153
Sean Yeoh <sean.yeoh@hotmail.com>
2017-07-12 10:58:09 +0800

    Update blank form and controller to include measurement info

--- app/controllers/blanks_controller.rb
+++ app/controllers/blanks_controller.rb
@@ -2,7 +2,8 @@ class BlanksController < ApplicationController
   dried_options ({
     fields:
       [:name, :brand, sides_attributes: [:id, :name, :attachment, :attachment_data, :_destroy], properties_attributes: [:id, :prop_type, :method, :name, :element_id, :back_element_id, :_destroy],
-        full_prints_attributes: [:id, :name, :element_id, :_destroy], sleeve_prints_attributes: [:id, :name, :left_half_element_id, :right_half_element_id, :_destroy]],
+        full_prints_attributes: [:id, :name, :element_id, :in_width, :in_height, :mm_width, :mm_height, :px_width, :px_height, :_destroy],
+        sleeve_prints_attributes: [:id, :name, :left_half_element_id, :right_half_element_id, :in_width, :in_height, :mm_width, :mm_height, :px_width, :px_height, :_destroy]],
     presentation:
       [:id, :name, :brand]
   })
