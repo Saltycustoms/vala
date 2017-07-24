156c9c53dc7a43901885f8b8722d8c0c8fe081e1
Sean Yeoh <sean.yeoh@hotmail.com>
2017-07-24 16:33:49 +0800

    Add currency to products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,9 @@
 class ProductsController < ApplicationController
   dried_options ({
     fields:
-      [:name, :blank_id, sizes_attributes: [:id, :name, :_destroy], color_ids: [],
+      [:name, :blank_id, :currency, sizes_attributes: [:id, :name, :_destroy], color_ids: [],
       price_ranges_attributes: [:id, :from_quantity, :to_quantity, :price_cents, :lower_rrp_cents, :higher_rrp_cents, :commission, :_destroy]],
     presentation:
-      [:id, :name]
+      [:id, :name, :currency]
   })
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
   belongs_to :blank
   validates :name, presence: true
   validates :blank_id, presence: true
+  validates :currency, presence: true
   has_many :sizes, dependent: :destroy
   accepts_nested_attributes_for :sizes, allow_destroy: true, reject_if: proc { |attributes| attributes.all? { |key, value| key == "_destroy" || value.blank? } }
   has_many :price_ranges, dependent: :destroy
