942160783c25a89bc969de38bdec14f3fb034da1
Sean Yeoh <sean.yeoh@hotmail.com>
2017-07-27 15:53:25 +0800

    Add sorting for size api controller

--- app/controllers/api/v1/sizes_controller.rb
+++ app/controllers/api/v1/sizes_controller.rb
@@ -1,2 +1,11 @@
 class Api::V1::SizesController < Api::V1::ApiV1Controller
+  def index
+    keys = params.keys.collect { |k| k if k.end_with?("id")}.compact
+    if keys
+      @resources = resource_class.where(build_options(keys))
+    else
+      @resources = resource_class.all
+    end
+    render json: @resources.sort_by {|size| Size.sort_array.index(size.name)}
+  end
 end
