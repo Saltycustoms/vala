563da4ae788031a32bb9d384c4ded9ec466dee2b
Sean Yeoh <sean.yeoh@hotmail.com>
2017-07-27 15:56:00 +0800

    Add pantone code to colors

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170727070124) do
+ActiveRecord::Schema.define(version: 20170727075424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,8 +34,9 @@ ActiveRecord::Schema.define(version: 20170727070124) do
   create_table "colors", force: :cascade do |t|
     t.string   "name"
     t.string   "hex"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.string   "pantone_code"
   end
 
   create_table "price_ranges", force: :cascade do |t|
