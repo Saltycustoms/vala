37ee7d7239ac87e7fab8d546eae00af83d97ae4f
LarryMckuydee <larry.mckuydee@gmail.com>
2017-09-05 16:32:22 +0800

    set public

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -7,13 +7,12 @@ if Rails.env.production?
     secret_access_key: "t2Y6XgP4KwOZtDEXUwWltNn3bPQsWuDzfQ2bJBMI",
     region:            "ap-southeast-1",
     bucket:            "thehand",
+    upload_options: { acl: "public-read" }
   }
 
   Shrine.storages = {
     cache: Shrine::Storage::S3.new(prefix: "athena_cache", **s3_options),
     store: Shrine::Storage::S3.new(prefix: "athena_store", **s3_options),
-    cache_control: "public, max-age=#{30.days}",
-    acl: "public-read"
   }
 
 else
@@ -24,17 +23,16 @@ else
     secret_access_key: "t2Y6XgP4KwOZtDEXUwWltNn3bPQsWuDzfQ2bJBMI",
     region:            "ap-southeast-1",
     bucket:            "thehand",
+    upload_options: { acl: "public-read" }
   }
 
   Shrine.storages = {
     cache: Shrine::Storage::S3.new(prefix: "athena_cache_dev", **s3_options),
     store: Shrine::Storage::S3.new(prefix: "athena_store_dev", **s3_options),
-    cache_control: "public, max-age=#{30.days}",
-    acl: "public-read"
   }
 end
 
-Shrine.plugin :default_url_options, store: { host: "https://thehand.s3.amazonaws.com" }
+Shrine.plugin :default_url_options, store: { public: true, host: "https://thehand.s3.amazonaws.com" }
 
 Shrine.plugin :activerecord
 Shrine.plugin :cached_attachment_data # for forms
