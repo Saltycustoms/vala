3af726d6dd3559a1e7ae5cc1020f27063e59e4e1
Sean Yeoh <sean.yeoh@hotmail.com>
2017-09-07 11:22:44 +0800

    Add money rails gem

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,10 @@
 class ProductsController < ApplicationController
   before_action :authorize_user, only: [:new, :create, :edit, :update, :destroy]
-  
+
   dried_options ({
     fields:
       [:name, :blank_id, :currency, :custom_dye, sizes_attributes: [:id, :name, :_destroy], color_ids: [],
-      price_ranges_attributes: [:id, :from_quantity, :to_quantity, :price_cents, :lower_rrp_cents, :higher_rrp_cents, :commission, :_destroy]],
+      price_ranges_attributes: [:id, :from_quantity, :to_quantity, :price, :lower_rrp, :higher_rrp, :commission, :_destroy]],
     presentation:
       [:name, :custom_dye, :currency]
   })
--- app/models/price_range.rb
+++ app/models/price_range.rb
@@ -1,10 +1,29 @@
 class PriceRange < ApplicationRecord
+  include MoneyRails::ActionViewExtension
   acts_as_paranoid
   belongs_to :product, optional: true
   validates :from_quantity, presence: true, numericality: {greater_than_or_equal_to: 1}
   validates :to_quantity, presence: true, numericality: {greater_than_or_equal_to: 1}
-  validates :price_cents, presence: true, numericality: {greater_than_or_equal_to: 1}
-  validates :lower_rrp_cents, numericality: {greater_than_or_equal_to: 1, allow_nil: true}
-  validates :higher_rrp_cents, numericality: {greater_than_or_equal_to: 1, allow_nil: true}
   validates :commission, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 1, allow_nil: true}
+  monetize :price_cents, with_model_currency: :currency, numericality: { greater_than_or_equal_to: 0 }
+  monetize :lower_rrp_cents, with_model_currency: :currency, numericality: { greater_than_or_equal_to: 0 }
+  monetize :higher_rrp_cents, with_model_currency: :currency, numericality: { greater_than_or_equal_to: 0 }
+
+  def currency
+    if product
+      product.currency
+    end
+  end
+
+  def price_amount
+    humanized_money_with_symbol price
+  end
+
+  def lower_rrp_amount
+    humanized_money_with_symbol lower_rrp
+  end
+
+  def higher_rrp_amount
+    humanized_money_with_symbol higher_rrp
+  end
 end
