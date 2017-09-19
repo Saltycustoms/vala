54d12565bb097e192e4a18a7bb02773776fa5dd1
Aiman Yusra <aimanyusra@hotmail.com>
2017-09-19 15:14:34 +0800

    Added down the collar and artwork measurement for sides

--- app/controllers/blanks_controller.rb
+++ app/controllers/blanks_controller.rb
@@ -3,7 +3,7 @@ class BlanksController < ApplicationController
 
   dried_options ({
     fields:
-      [:name, :brand, sides_attributes: [:id, :name, :attachment, :attachment_data, :_destroy], properties_attributes: [:id, :prop_type, :method, :name, :element_id, :back_element_id, :_destroy],
+      [:name, :brand, sides_attributes: [:id, :name, :attachment, :attachment_data, :_destroy], properties_attributes: [:id, :prop_type, :method, :name, :element_id, :back_element_id, :hex, :_destroy],
         full_prints_attributes: [:id, :name, :element_id, :in_width, :in_height, :mm_width, :mm_height, :px_width, :px_height, :_destroy],
         sleeve_prints_attributes: [:id, :name, :left_half_element_id, :right_half_element_id, :in_width, :in_height, :mm_width, :mm_height, :px_width, :px_height, :_destroy],
         woven_label_prints_attributes: [:id, :name, :element_id, :in_width, :in_height, :mm_width, :mm_height, :px_width, :px_height, :_destroy]],
