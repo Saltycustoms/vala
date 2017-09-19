fcd00491e15516b576d6ead38d58ee9262229cd4
Aiman Yusra <aimanyusra@hotmail.com>
2017-09-19 14:40:09 +0800

    Added hex input for property

--- app/models/blank.rb
+++ app/models/blank.rb
@@ -10,6 +10,8 @@ class Blank < ApplicationRecord
   accepts_nested_attributes_for :full_prints, allow_destroy: true, reject_if: proc { |attributes| attributes.all? { |key, value| key == "_destroy" || value.blank? } }
   has_many :sleeve_prints, class_name: "Print::SleevePrint", dependent: :destroy
   accepts_nested_attributes_for :sleeve_prints, allow_destroy: true, reject_if: proc { |attributes| attributes.all? { |key, value| key == "_destroy" || value.blank? } }
+  has_many :woven_label_prints, class_name: "Print::WovenLabel", dependent: :destroy
+  accepts_nested_attributes_for :woven_label_prints, allow_destroy: true, reject_if: proc { |attributes| attributes.all? { |key, value| key == "_destroy" || value.blank? } }
   validates :name, presence: true
   validates :brand, presence: true
 end
